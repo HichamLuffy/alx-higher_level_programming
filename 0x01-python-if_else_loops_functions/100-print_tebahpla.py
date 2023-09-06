dbca8815c7a14027c3caff7e7d4daa54a01da3ab
hicham <imalxvssteve2020@gmail.com>
2023-09-06 01:02:19 +0100

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-for alpha in range(ord('z'), ord('a')-1, -1):
+def uppercase(str):
+    rest_str = ""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            rest_str += chr(ord(c) - 32)
+        else:
+            rest_str += c
+    return rest_str
+
+for alpha in range(ord('z'), ord('a') - 1, -1):
+    if alpha % 2 != 0:
+        alpha = ord(uppercase(chr(alpha)))
     print("{}".format(chr(alpha)), end="")
