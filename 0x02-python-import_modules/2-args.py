437d46c8539c6fc54a4f4c8fc6d0dabd0863e396
hicham <imalxvssteve2020@gmail.com>
2023-09-07 17:40:40 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+
+    len = len(argv)-1
+    if (len == 0):
+        print("{} arguments:".format(len))
+    else:
+        print("{} arguments:".format(len))
+    for i in range(1, len+1, 1):
+        print("{}: {}".format(i, argv[i]))
