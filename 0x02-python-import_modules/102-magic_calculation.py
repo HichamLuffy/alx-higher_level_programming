10f532bfe8ac9232193ad8661dcb29a13e26e082
hicham <imalxvssteve2020@gmail.com>
2023-09-07 21:19:04 +0100

    task 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    c = 0
+    from calculator_1 import add, sub
+
     if a < b:
-        c = a + b
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
     else:
-        c = a * b
-    return c
+        return(sub(a, b))
