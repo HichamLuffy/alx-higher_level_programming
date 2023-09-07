30c782b2f3497b642e40b55bdfe4f0d9b96ab513
hicham <imalxvssteve2020@gmail.com>
2023-09-07 18:04:07 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ from sys import argv
 
 if __name__ == "__main__":
     len = len(argv)-1
-    if (len == 0):
+    if len == 0:
         print("0 arguments.")
-    elif(len == 1):
+    elif len == 1:
         print("{} argument:\n{}: {}".format(len, len, argv[len]))
     else:
         print("{} arguments:".format(len))
