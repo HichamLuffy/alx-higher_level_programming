408263d69a5b5b137bb0f9b5be7c2c0839f5cc03
hicham <imalxvssteve2020@gmail.com>
2023-09-07 20:58:30 +0100

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from unittest import case
     from calculator_1 import *
     from sys import argv
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-    if operator == "+":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        operator = str(argv[2])
+        b = int(argv[3])
+        if operator == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
+        elif operator == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
+        elif operator == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == "/":
+        elif operator == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
-
+        else:
+            exit (1)
