aff1cc75d400b303f4916ae2a05ce4f3d080dc79
hicham <imalxvssteve2020@gmail.com>
2023-09-07 18:53:37 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     len = len(argv)-1
-    if len == 0:
-        print("0")
     x = 0
-    for i in range(1, len+1):
-        x += int(argv[i])
-    print("{:d}".format(x))
+    if len >= 2:
+        for i in range(1, len+1):
+            x += int(argv[i])
+        print("{:d}".format(x))
+    else:
+        print("{:d}".format(x))
