57871234cfb74f7896ef20350f5fde4ae017a9d0
hicham <imalxvssteve2020@gmail.com>
2023-09-07 17:47:16 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     len = len(argv)-1
     if (len == 0):
-        print("{} arguments.".format(len))
+        print("{} arguments.".format(0))
     else:
         print("{} arguments:".format(len))
     for i in range(1, len+1, 1):
