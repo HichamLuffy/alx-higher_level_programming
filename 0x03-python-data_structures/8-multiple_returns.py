39c86591cedc72797f0a47460a20b2662fa334b8
hicham <imalxvssteve2020@gmail.com>
2023-09-10 20:02:27 +0100

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
     if sentence is None:
-        return length, None
+        return 0, None
+    length = len(sentence)
     first = sentence[0]
     return length, first
