9812f77ccfb5fb6c0df03e551d5258a873ef26cd
hicham <imalxvssteve2020@gmail.com>
2023-09-13 20:06:09 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    cpy_matrix = matrix.copy()
+    for i in cpy_matrix:
+        for j in range(len(i)):
+            i[j] = i[j] ** 2
+    return cpy_matrix
