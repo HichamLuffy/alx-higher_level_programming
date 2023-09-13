6993745454cf56aaf6cddc53cfb3dfe8c72cdcf7
hicham <imalxvssteve2020@gmail.com>
2023-09-13 22:19:33 +0100

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    cpy_dictionary = a_dictionary.copy()
+    for key, value in cpy_dictionary.items():
+        cpy_dictionary[key] = value * 2
+    return cpy_dictionary
