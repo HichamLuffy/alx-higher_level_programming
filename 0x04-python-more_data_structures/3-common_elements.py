5b1d1223ec12a15065b901ea91b6b2f6e9024c35
hicham <imalxvssteve2020@gmail.com>
2023-09-13 20:45:03 +0100

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    set_3 = ""
+    for i in set_1:
+        for j in set_2:
+            if j == i:
+                set_3 = set_3 + j
+    return set_3
