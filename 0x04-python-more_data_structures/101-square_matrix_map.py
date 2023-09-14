ba7ed056086233c77da48c33d96724ebf767d248
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-14 02:36:14 +0100

    task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     cpy_matrix = [list(row) for row in matrix]
-    sum = list(map(lambda x: list(map(lambda y: y ** 2, x)), cpy_matrix))
-    return sum
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), cpy_matrix))
