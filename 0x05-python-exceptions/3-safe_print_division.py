f74fc1e19069ba2d94bba023b7868f5860fa37df
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-25 16:56:22 +0100

    task 1

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         inside = a / b
-        print("Inside result: = {:.2f}".format(inside))
-        return inside
     except (TypeError, ZeroDivisionError):
-        print("Inside result: = None")
-        return None
+        inside = None
+    finally:
+        print("inside result = {}".format(inside))
+        return inside
