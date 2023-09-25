9e66d8138fef40dd8be07dde35a58f8d455725c5
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-25 23:21:45 +0100

    task 101

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    rslt = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            rslt += a ** b / i
+        except Exception:
+            rslt = b + a
+            break
+    return rslt
