337959a8b39e2be33597667423aaf924fe97996a
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-25 15:12:01 +0100

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(value)
         print("{:d}".format(value))
         
-    except IndexError:
-        print("")
+        return True
     except Exception:
         print("{} is not an integer".format(value))
+        
     return True
