53504b3c8347048a942124916459c80b088ad311
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-25 10:51:58 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         j = 0
         list_copy = my_list.copy()
         while x > 0:
-            print(list_copy[j], end='')
+            print("{}".format(list_copy[j]), end='')
             j += 1
             x -= 1
         print(" ")
