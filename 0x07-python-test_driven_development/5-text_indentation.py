48572d4569fe0bd710f546443949ade719d38829
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-01 15:58:22 +0100

    task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    alpha = 0
+    while alpha < len(text) and text[alpha] == ' ':
+        alpha += 1
+    while alpha < len(text):
+        print(text[alpha], end="")
+        if text [alpha] == '\n' or text[alpha] == '?' or text[alpha] == '.' or text[alpha] == ':':
+            print('\n')
+            alpha += 1
+            while alpha < len(text) and text[alpha] == " ":
+                alpha += 1
+            continue
+        alpha += 1
