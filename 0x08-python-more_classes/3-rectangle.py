2987c40b7796c92a2c351ac266b30ae7131b75dd
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-02 15:27:52 +0100

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -45,8 +45,5 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         for i in range(self.__height):
-            if i != self.__height - 1:
-                print("#" * self.__width)
-            else:
-                print("#" * self.__width, end="")
+            print("#" * self.__width)
         return ""
