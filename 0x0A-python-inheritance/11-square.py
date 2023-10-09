0883e9b89789b68899a73a39879cfd01e657ae7d
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 00:19:18 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
@@ -14,6 +13,6 @@ class Square(Rectangle):
 
     def area(self):
         return self.__size * self.__size
-    
+
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
