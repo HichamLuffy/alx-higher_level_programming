5a178461314fe6b0c14e20bec629cef6cceaee0a
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 00:34:19 +0100

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     """MyInt"""
     def __eq__(self, value):
         """__eq__"""
-        return self.real == value
+        return self.real != value
     
     def __ne__(self, value):
         """__ne__"""
-        return self.real != value
+        return self.real == value
