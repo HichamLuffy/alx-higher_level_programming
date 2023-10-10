e92957b2a42fc7721d386638651d1c187ef616c5
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 20:32:38 +0100

    task 0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""write_file"""
 
 
 def write_file(filename="", text=""):
+    """write_file"""
     with open(filename, "w", encoding="utf-8") as f:
-    return f.write(text)
+        return f.write(text)
