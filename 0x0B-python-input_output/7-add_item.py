b0238eadd0a5f84440191877df6ed5646d5729ff
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 22:30:23 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    list = load_from_json_file("add_item.json")
-except Exception:
-    list = []
-list.extend(sys.argv[1:])
-save_to_json_file(list, "add_item.json")
+    try:
+        list = load_from_json_file("add_item.json")
+    except Exception:
+        list = []
+    list.extend(sys.argv[1:])
+    save_to_json_file(list, "add_item.json")
