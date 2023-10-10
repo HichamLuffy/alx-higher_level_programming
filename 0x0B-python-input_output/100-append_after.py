9a74fa60831594331726d682086aeefb3e19c660
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 23:49:14 +0100

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""appden after a string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appden after a string"""
+    with open(filename, "r") as f:
+        text = f.read()
+    text = text.replace(search_string, new_string)
+    with open(filename, "w") as f:
+        f.write(text)
