fbf81d46df85c4dbced263aa87f9eb28633349bf
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-10 23:06:05 +0100

    task 10

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """to json"""
         return {
             'first_name': self.first_name,
