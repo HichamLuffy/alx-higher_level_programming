a12b1df36a44be6edcdaecfc057d819028b5df24
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-13 23:12:54 +0100

    task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,9 +72,31 @@ class Rectangle(Base):
             for k in range(self.__width):
                 print("#", end="")
             print()
-    
+
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    
+    def update(self, *args):
+        if len(args) == 0:
+            return
+        elif len(args) == 1:
+            self.id = args[0]
+        elif len(args) == 2:
+            self.id = args[0]
+            self.width = args[1]
+        elif len(args) == 3:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+        elif len(args) == 4:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+        elif len(args) == 5:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
