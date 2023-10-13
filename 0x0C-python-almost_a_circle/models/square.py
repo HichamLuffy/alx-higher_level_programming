334d917e8d2a615568e43347d5e7b9df43481e8c
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-14 00:24:10 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,3 +7,12 @@ class Square(Rectangle):
     """square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
