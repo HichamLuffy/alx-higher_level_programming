5adb537823cdd5eb1451ea9eb797d35ce0eae87a
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-16 18:32:55 +0100

    comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ from models.base import Base
 class Rectangle(Base):
     """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init"""
+        """ init """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,7 +17,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """width"""
+        """ width """
         return self.__width
 
     @width.setter
