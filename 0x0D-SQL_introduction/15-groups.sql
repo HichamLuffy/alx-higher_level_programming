0896a226205bb95ebba83e8c2b16397da58adbea
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-15 00:23:02 +0100

    updates comments

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
--- task 15
+-- lists the number of records with the same score in the table second_table
 SELECT score, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- task 16
+-- lists all records of the table second_table of the database hbtn_0c_0
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
