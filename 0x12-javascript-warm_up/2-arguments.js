316cea5d8c97b6f299e4022dd8da7a8900d23527
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-12 00:27:36 +0100

    arguments fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
+
 if (length === 2) {
     console.log('No argument');
-  }
-  if (length === 3) {
+}
+if (length === 3) {
     console.log('Argument found');
-  }
-  if (length > 3) {
+}
+if (length > 3) {
     console.log('Arguments found');
-  }
+}
