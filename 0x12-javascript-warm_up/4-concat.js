fc25a2b6ac8f50a620b5fff33157fd58ec02b3d4
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-12 00:59:18 +0100

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const myNumber = 'My number: ';
-if (!isNaN(parseInt(argv[2], 10))) {
-  console.log(myNumber + parseInt(argv[2], 10));
-} else console.log('Not a number');
+const firstArgv = argv[2];
+const secondArgv = argv[3];
+const middleString = ' is ';
+
+console.log(firstArgv + middleString + secondArgv);
