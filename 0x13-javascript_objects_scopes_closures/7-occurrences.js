1291046d0041f9fff823f4043783f81cd2777d9f
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-12 19:17:15 +0100

    fix square#2

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) { 
-      counter++; 
+    if (list[i] === searchElement) {
+      counter++;
     }
   }
   return counter;
-};
+}
