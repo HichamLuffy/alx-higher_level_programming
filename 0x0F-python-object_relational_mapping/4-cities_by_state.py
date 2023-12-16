72f9ea08768312dc09ba932cc7ecdd60b2c9fb32
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-16 01:49:34 +0100

    cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""select states"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) < 4:
+        print("wrong format")
+        exit()
+    # take arguments
+    db_user = sys.argv[1]
+    db_pass = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", user=db_user, passwd=db_pass, db=db_name, port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON states.id = cities.state_id \
+    ORDER BY cities.id")
+
+    # fetch all
+    rows = cursor.fetchall()
+
+    # Display all
+    for row in rows:
+        print(row)
+
+    # close connection
+    cursor.close()
+    db.close()
