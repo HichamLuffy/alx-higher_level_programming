eebd4225ad0abbfe1a230b9060006641f7b5a99c
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-16 23:12:56 +0100

    fix task 5 filter_cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""cities by state"""
+"""filter cities"""
 
 
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    if len(sys.argv) < 5:
-        print("wrong format")
-        exit()
     # take arguments
     db_user = sys.argv[1]
     db_pass = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    query = "SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON states.id = cities.state_id \
-    WHERE states.name = '{}' ".format(state_name)
+    query = "SELECT name FROM cities WHERE state_id = \
+    (SELECT id FROM states WHERE name = '{}' LIMIT 1) ORDER BY id ASC".format(state_name)\
 
     db = MySQLdb.connect(
         host="localhost", user=db_user, passwd=db_pass, db=db_name, port=3306
@@ -29,8 +25,10 @@ if __name__ == "__main__":
     rows = cursor.fetchall()
 
     # Display all
+    tuples = []
     for row in rows:
-        print(row)
+        tuples += row
+    print(", ".join(tuples))
 
     # close connection
     cursor.close()
