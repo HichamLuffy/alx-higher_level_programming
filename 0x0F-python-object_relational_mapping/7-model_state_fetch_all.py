f76bad82c690b7c88a8d1c657d089791c50bda35
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-17 17:13:57 +0100

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,42 +1,24 @@
 #!/usr/bin/python3
-#venv\Scripts\activate
-import turtle
-from tkinter import PhotoImage
+"""Start link class to table in database
+"""
 
-# Function to draw a heart
-def draw_heart():
-    turtle.fillcolor("red")
-    turtle.begin_fill()
-    turtle.left(50)
-    turtle.forward(133)
-    turtle.circle(50, 200)
-    turtle.right(140)
-    turtle.circle(50, 200)
-    turtle.forward(133)
-    turtle.end_fill()
 
-# Function to write "I love you" inside the heart
-def write_love_phrase():
-    turtle.penup()
-    turtle.goto(0, -50)
-    turtle.color("white")
-    turtle.write("I love you", align="center", font=("Arial", 16, "normal"))
-
-# Main function
-def main():
-    turtle.speed(2)
-    turtle.bgcolor("black")
-    turtle.color("red")
-    draw_heart()
-    write_love_phrase()
-
-    # Get the canvas and save it to an image file
-    canvas = turtle.getcanvas()
-    canvas.postscript(file="heart_image.ps", colormode='color')
-
-    turtle.hideturtle()
-    turtle.done()
-
-# Run the main function
 if __name__ == "__main__":
-    main()
+    """ Start link class to table in database"""
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine, select
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc())
+        states = connection.execute(query)
+        for state in states:
+            print(f"{state.id}: {state.name}")
+
+    engine.dispose()
