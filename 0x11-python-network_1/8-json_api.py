23b51ef7ca41f81559c2de724e1894816094c34f
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:15:06 +0100

    API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""API"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    data = {'q': q}
+    res = requests.post(url, data)
+    try:
+        json_response = res.json()
+        if 'id' in json_response and 'name' in json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
