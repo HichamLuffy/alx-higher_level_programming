de09fed9051d8e8f00b63461738f7a281e369a13
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 19:59:35 +0100

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Post an Email"""
+from sys import argv
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    email = {'email': argv[2]}
+    data = urllib.parse.urlencode(email).encode('utf-8')
+    req = urllib.request.Request(url, data, method='POST')
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
