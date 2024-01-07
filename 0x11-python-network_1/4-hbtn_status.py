6e440b1b698d7e705b370993c6c15cffa323b7ae
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:06:52 +0100

    hbtn status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" HBTN Status """
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+    data = res.text
+    resquest_type = type(data)
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(resquest_type, data))
