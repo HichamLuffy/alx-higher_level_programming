cd3c74e747c0e5b5a452138e7e6d0fa976734926
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:00:34 +0100

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL and displays the Error code"""
+from sys import argv
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
