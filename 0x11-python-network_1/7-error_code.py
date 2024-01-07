69c10ee068c93b53e4ad51630598e0dd5d36870f
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:13:39 +0100

    error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" displays the Error code"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        print(res.text)
+    except requests.HTTPError as e:
+        print(f"Error code: {e.response.status_code}")
