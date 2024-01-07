4c785734ed3feb99b93341d43bcfd84b703e5728
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 19:57:47 +0100

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Get the value of X-Request-Id in the header of the response """
+from sys import argv
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as r:
+        print(r.headers.get('X-Request-Id'))
