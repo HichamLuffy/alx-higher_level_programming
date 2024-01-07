43bd3435642501d587cecf63c5732a3f800efd25
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:16:49 +0100

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Time for an interview!"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits?per_page={}'
+    url = url.format(
+        argv[2], argv[1], 10
+    )
+    r = requests.get(url)
+    commits = r.json()
+    for c in commits:
+        print("{}: {}"
+              .format(c.get('sha'), c.get('commit').get('author').get('name')))
