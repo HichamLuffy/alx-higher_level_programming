cf169b50d4d6a61ad5b05b1f5bdce522e1db3093
HichamLuffy <imalexvssteve2020@gmail.com>
2024-01-07 20:12:08 +0100

    hbtn header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Get the value of X-Request-Id in the header """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = argv[1]
+    res = requests.get(url)
+    print(res.headers.get('X-Request-Id'))
